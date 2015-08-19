2485aa004c77887b3114c03a0c3fa2e720481f27
Frank Vaernewyck <frank.vaernewyck@ordina.be>
2015-08-19 15:13:26 +0200

    Filters splitted in 2 files

--- src/app/graph/graph.node.filter.js
+++ src/app/graph/graph.node.filter.js
@@ -30,23 +30,5 @@ angular.module('microServicesGui')
       }
       return filteredNodes;
     };
-  })
-  .filter('linkFilter', function () {
-    return function (links, nodes) {
-      var filteredLinks = new MSGSet();
-      for (var i = 0; i < links.length; i++) {
-        for (var j = 0; j < nodes.length; j++) {
-          if (links[i].source.id === nodes[j].id) {
-            for (var k = 0; k < nodes.length; k++) {
-              if (links[i].target.id === nodes[k].id) {
-                filteredLinks.add(links[i]);
-                break;
-              }
-            }
-          }
-        }
-      }
-      return filteredLinks.values();
-    };
   });
 })();
