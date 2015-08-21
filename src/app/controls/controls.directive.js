981d6b6f689ee05c6d11c9cf9e78fce70f533d60
Frank Vaernewyck <frank.vaernewyck@ordina.be>
2015-08-21 13:53:24 +0200

    Filters only show the options available (0 counts are not shown)

--- src/app/controls/controls.directive.js
+++ src/app/controls/controls.directive.js
@@ -25,8 +25,18 @@
 
         scope.$watch('countArray.length', function () {
           scope.states = GraphService.getStates();
+
+          var i = scope.states.length;
+          while (i--) {
+            var state = scope.states[i];
+            if (countState(state) == 0) {
+              scope.states.splice(i, 1);
+            }
+          }
+
           scope.states.forEach(function (state, i) {
             scope.states[i].value = state.value + " - " + scope.countArray.filter(function (d) {
+                console.log("state:" + scope.countArray.length);
                 if (state.value === 'ALL') {
                   return true;
                 }
@@ -36,23 +46,47 @@
                 return false;
 
               }).length
-          })
+          });
+
           scope.types = GraphService.getTypes();
+
+          i = scope.types.length;
+          console.log("i=" + i);
+          while (i--) {
+            var type = scope.types[i];
+            if (countType(type) == 0) {
+              scope.types.splice(i, 1);
+            }
+          }
+
           scope.types.forEach(function (type, i) {
             scope.types[i].value = type.value + " - " + scope.countArray.filter(function (d) {
-                if (type.value === 'ALL') {
-                  return true;
+                  console.log("type:" + scope.countArray.length);
+                  if (type.value === 'ALL') {
+                    return true;
+                  }
+                  if (d.details != undefined) {
+                    return d.details.type === type.value;
+                  }
+                  return false;
                 }
-                if (d.details != undefined) {
-                  return d.details.type === type.value;
-                }
-                return false;
+              ).length
+          });
 
-              }).length
-          })
           scope.groups = GraphService.getGroups();
+
+          i = scope.groups.length;
+          console.log("i=" + i);
+          while (i--) {
+            var group = scope.groups[i];
+            if (countGroup(group) == 0) {
+              scope.groups.splice(i, 1);
+            }
+          }
+
           scope.groups.forEach(function (group, i) {
             scope.groups[i].value = group.value + " - " + scope.countArray.filter(function (d) {
+                console.log("group:" + scope.countArray.length);
                 if (group.value === 'ALL') {
                   return true;
                 }
@@ -68,12 +102,46 @@
         scope.clearFilter = function () {
           scope.nodeSearch.id = "";
 
-          scope.nodeSearch.status = 'ALL';
-          scope.nodeSearch.type = 'ALL';
-          scope.nodeSearch.group = 'ALL';
+          scope.nodeSearch.status = null;
+          scope.nodeSearch.type = null;
+          scope.nodeSearch.group = null;
         };
+
+        function countState(state) {
+          return scope.countArray.filter(function (d) {
+            if (d.details != undefined) {
+              if (state.key === d.details.status) {
+                return true;
+              }
+            }
+            return false;
+          }).length
+        }
+
+        function countType(type) {
+          return scope.countArray.filter(function (d) {
+            if (d.details != undefined) {
+              if (type.key === d.details.type) {
+                return true;
+              }
+            }
+            return false;
+          }).length
+        }
+
+        function countGroup(group) {
+          return scope.countArray.filter(function (d) {
+            if (d.details != undefined) {
+              if (group.key === d.details.group) {
+                return true;
+              }
+            }
+            return false;
+          }).length
+        }
       }
     };
   }
+
 
 })();
