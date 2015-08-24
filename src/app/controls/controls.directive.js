119572a0baef12ab51c1a16446ae1d6ee81e2f4c
Frank Vaernewyck <frank.vaernewyck@ordina.be>
2015-08-24 11:56:55 +0200

    Handled code warnings

--- src/app/controls/controls.directive.js
+++ src/app/controls/controls.directive.js
@@ -13,7 +13,7 @@
       link: function (scope, elem, attrs, graphController) {
         scope.$watchGroup(['nodeSearch.id', 'nodeSearch.status', 'nodeSearch.type', 'nodeSearch.group'], function () {
           graphController.filterNodes(scope.nodeSearch);
-        })
+        });
 
         scope.states = GraphService.getStates();
         scope.types = GraphService.getTypes();
@@ -29,74 +29,74 @@
           var i = scope.states.length;
           while (i--) {
             var state = scope.states[i];
-            if (countState(state) == 0) {
+            if (countState(state) === 0) {
               scope.states.splice(i, 1);
             }
           }
 
           scope.states.forEach(function (state, i) {
             scope.states[i].value = state.value + " - " + scope.countArray.filter(function (d) {
-                console.log("state:" + scope.countArray.length);
+//                console.log("state:" + scope.countArray.length);
                 if (state.value === 'ALL') {
                   return true;
                 }
-                if (d.details != undefined) {
+                if (d.details !== undefined) {
                   return d.details.status === state.value;
                 }
                 return false;
 
-              }).length
+              }).length;
           });
 
           scope.types = GraphService.getTypes();
 
           i = scope.types.length;
-          console.log("i=" + i);
+//          console.log("i=" + i);
           while (i--) {
             var type = scope.types[i];
-            if (countType(type) == 0) {
+            if (countType(type) === 0) {
               scope.types.splice(i, 1);
             }
           }
 
           scope.types.forEach(function (type, i) {
             scope.types[i].value = type.value + " - " + scope.countArray.filter(function (d) {
-                  console.log("type:" + scope.countArray.length);
+//                  console.log("type:" + scope.countArray.length);
                   if (type.value === 'ALL') {
                     return true;
                   }
-                  if (d.details != undefined) {
+                  if (d.details !== undefined) {
                     return d.details.type === type.value;
                   }
                   return false;
                 }
-              ).length
+              ).length;
           });
 
           scope.groups = GraphService.getGroups();
 
           i = scope.groups.length;
-          console.log("i=" + i);
+//          console.log("i=" + i);
           while (i--) {
             var group = scope.groups[i];
-            if (countGroup(group) == 0) {
+            if (countGroup(group) === 0) {
               scope.groups.splice(i, 1);
             }
           }
 
           scope.groups.forEach(function (group, i) {
             scope.groups[i].value = group.value + " - " + scope.countArray.filter(function (d) {
-                console.log("group:" + scope.countArray.length);
+//                console.log("group:" + scope.countArray.length);
                 if (group.value === 'ALL') {
                   return true;
                 }
-                if (d.details != undefined) {
+                if (d.details !== undefined) {
                   return d.details.group === group.value;
                 }
                 return false;
 
-              }).length
-          })
+              }).length;
+          });
         });
 
         scope.clearFilter = function () {
@@ -109,35 +109,35 @@
 
         function countState(state) {
           return scope.countArray.filter(function (d) {
-            if (d.details != undefined) {
+            if (d.details !== undefined) {
               if (state.key === d.details.status) {
                 return true;
               }
             }
             return false;
-          }).length
+          }).length;
         }
 
         function countType(type) {
           return scope.countArray.filter(function (d) {
-            if (d.details != undefined) {
+            if (d.details !== undefined) {
               if (type.key === d.details.type) {
                 return true;
               }
             }
             return false;
-          }).length
+          }).length;
         }
 
         function countGroup(group) {
           return scope.countArray.filter(function (d) {
-            if (d.details != undefined) {
+            if (d.details !== undefined) {
               if (group.key === d.details.group) {
                 return true;
               }
             }
             return false;
-          }).length
+          }).length;
         }
       }
     };
