5da9f5d5aa42106b300284fd8c2a0c0df73df31b
Dimitri De Kerf <dimitri.dekerf@ordina.be>
2015-08-24 16:31:41 +0200

    Unit testing NodeModalFilter

---
+++ src/app/nodemodal/nodemodal.filter.spec.js
@@ -0,0 +1,27 @@
+(function() {
+  'use strict';
+
+  describe("NodeModalFilter", function() {
+
+    beforeEach(module('microServicesGui'));
+
+    it('should have a node filter', inject(function($filter) {
+      expect($filter('nodeModalFilter')).toBeDefined();
+    }));
+
+    it('should have a node filter that produces an array of nodes',
+      inject(function($filter) {
+
+        var node1 = {id:'node1'};
+        var node2 = {id:'node2'};
+        var node3 = {id:'node3'};
+        var nodesList = [node1, node2, node3];
+        var linkedNode = {id: 'node2'};
+
+        var nodes = $filter('nodeModalFilter')(nodesList, linkedNode);
+
+        expect(nodes.length).toEqual(1);
+        expect(nodes[0]).toEqual(node2);
+      }));
+  });
+})();
