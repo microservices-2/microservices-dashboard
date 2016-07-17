cc7da7d35c38edb37c487a40e6f59ca37c873abb
Ryan de Gruyter <degruyter.ryan@gmail.com>
2016-07-17 08:28:15 +0200

    add codacy rule to eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   "extends": "google",
   "rules": {
     "object-curly-spacing": "off",
-    "one-var": "off"
+    "one-var": "off",
+    "no-use-before-define": ["error", { "functions": true, "classes": true }],
   }
 };
