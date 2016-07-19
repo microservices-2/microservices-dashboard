aae42b76ca92532a4449eb28d073d455078993e0
Ryan de Gruyter <degruyter.ryan@gmail.com>
2016-07-19 09:04:14 +0200

    refactor graph controller

--- src/app/graph/graph.controller.js
+++ src/app/graph/graph.controller.js
@@ -9,74 +9,89 @@
 
   /** @ngInject */
   function GraphController(
-    $scope, $rootScope, $filter, $q, GraphService, NodecolorService
+    $scope, $rootScope, $filter, $q, GraphService
   ) {
+    // view model
     var vm = this;
-    var nodesData;
-    var linksData;
-    var resultData;
+    vm.beFilter = {};
+    vm.graphData = undefined;
 
-    $scope.showLegend = { height: '0' };
-    $scope.beFilter = {};
+    // private controller state
+    var nodes;
+    var withFilter;
+
+    activate();
+    addOnNodeChangesListener();
+    addFilterWatch();
+
+    function activate() {
+      $rootScope.dataLoading = true;
+
+      GraphService.getGraph()
+        .then(parseGraphResponse);
+    }
+
+    function parseGraphResponse(response) {
+      var graphData = response.data;
+
+      graphData.nodes.forEach(function(node, index) {
+        node.index = index;
+        graphData.links.forEach(function(link) {
+          if (link.source === node.index) {
+            link.source = node;
+          }
+          if (link.target === node.index) {
+            link.target = node;
+          }
+        });
+      });
+
+      nodes = graphData.nodes;
+
+      if (withFilter) {
+        vm.graphData = applyFilters(graphData);
+      } else {
+        graphData.links = $filter('linkFilter')(graphData.links, graphData.nodes);
+        vm.graphData = graphData;
+      }
+
+      $rootScope.dataLoading = false;
+    }
 
     function applyFilters(data) {
       data.links = $filter('linkFilter')(data.links, data.nodes);
-      data.nodes = $filter('nodeFilter')(data.nodes, $scope.beFilter);
-      var cf = $filter('cascadingFilter2')(data.links, nodesData, data.nodes);
+      data.nodes = $filter('nodeFilter')(data.nodes, vm.beFilter);
+
+      var cf = $filter('cascadingFilter2')(data.links, nodes, data.nodes);
       data.nodes = cf.nodes;
       data.links = cf.links;
+
       return data;
     }
-    function isUndefinedEmptyOrNull(obj) {
-      return (typeof obj === 'undefined' || obj === null || obj === '');
-    }
-    function init(withFilter) {
-      $rootScope.dataLoading = true;
-      GraphService.getGraph().then(function(values) {
-        resultData = values.data;
-        resultData.nodes.forEach(function(node, index) {
-          node.index = index;
-          resultData.links.forEach(function(d) {
-            if (d.source === node.index) {
-              d.source = node;
-            }
-            if (d.target === node.index) {
-              d.target = node;
-            }
-          });
-        });
 
-        nodesData = resultData.nodes;
-        linksData = resultData.links;
-
-        if (withFilter) {
-          $scope.graphData = applyFilters(resultData);
-        } else {
-          resultData.links = $filter('linkFilter')(resultData.links, resultData.nodes);
-          $scope.graphData = resultData;
+    function addFilterWatch() {
+      $scope.$watch('vm.beFilter', function(value, prev) {
+        if (!angular.equals({}, prev) && angular.isDefined(prev)) {
+          if (isUndefinedEmptyOrNull(value.details.type) && isUndefinedEmptyOrNull(value.details.group) && isUndefinedEmptyOrNull(value.details.status) && isUndefinedEmptyOrNull(value.id)) {
+            withFilter = false;
+            activate();
+          } else {
+            withFilter = true;
+            activate();
+          }
         }
-        $rootScope.dataLoading = false;
+      }, true);
+    }
+
+    function addOnNodeChangesListener() {
+      $rootScope.$on('nodesChanged', function() {
+        withFilter = true;
+        activate();
       });
     }
 
-    $scope.$watch('beFilter', function(value, prev) {
-      if (!angular.equals({}, prev)) {
-        if (isUndefinedEmptyOrNull(value.details.type) && isUndefinedEmptyOrNull(value.details.group) && isUndefinedEmptyOrNull(value.details.status) && isUndefinedEmptyOrNull(value.id)) {
-          init();
-        } else {
-          init(true);
-        }
-      }
-    }, true);
-
-    $rootScope.$on('nodesChanged', function() {
-      init(true);
-    });
-
-    init();
-
-    $scope.getColor = function(node) {
-      return { 'background-color': String(NodecolorService.getColorFor(node)) };
-    };
+    function isUndefinedEmptyOrNull(obj) {
+      return (typeof obj === 'undefined' || obj === null || obj === '');
+    }
   }
 })();
