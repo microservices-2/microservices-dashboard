46901cec3d49a625b53f6d85f73fb71066c302ce
Ryan de Gruyter <degruyter.ryan@gmail.com>
2016-07-22 10:14:59 +0200

    add (test) getter and setter event list

--- src/app/events/events.service.js
+++ src/app/events/events.service.js
@@ -8,24 +8,33 @@
 
   Service.$inject = ['$http', 'BASE_URL'];
   /**
-   * Responsible for managing the event list local state
+   * Responsible for managing the local event list state.
    * And managing the HTTP RESOURCE /events
    *
    * @param {any} $http
    * @param {any} BASE_URL
    */
   function Service($http, BASE_URL) {
-    var self = this;
+    var _self = this;
 
-    self.request = request;
-    self.eventList = [];
+    _self.request = request;
+    _self.getEventList = getEventList;
+    _self.setEventList = setEventList;
+    _self.eventList = undefined;
+
     // //////////////
+    function setEventList(list) {
+      _self.eventList = list;
+    }
+    function getEventList() {
+      return _self.eventList;
+    }
     function request() {
       return $http
         .get(BASE_URL + 'events')
         .then(function(response) {
           if (response.data) {
-            self.eventList = response.data;
+            _self.eventList = response.data;
           }
         });
     }
