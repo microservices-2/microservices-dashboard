b3ad92b16aa2a6b6eb3af75c0069711d6a4aade3
Andreas Evers <krosan@gmail.com>
2018-11-22 15:49:55 +0100

    Added integration test

---
+++ microservices-dashboard-server/src/test/java/be/ordina/msdashboard/MicroservicesDashboardServerApplicationTest.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2012-2016 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package be.ordina.msdashboard;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
+
+/**
+ * Tests for the Microservices Dashboard server application
+ *
+ * @author Andreas Evers
+ */
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = RANDOM_PORT,
+		classes = MicroservicesDashboardServerApplicationTest.TestMicroservicesDashboardServerApplication.class)
+@DirtiesContext
+public class MicroservicesDashboardServerApplicationTest {
+
+	private static final Logger logger = LoggerFactory.getLogger(MicroservicesDashboardServerApplicationTest.class);
+
+	@Value("${local.server.port}")
+	private int port = 0;
+
+	@Test
+	public void contextLoads() {
+
+	}
+
+	@Configuration
+	@EnableAutoConfiguration
+	@EnableMicroservicesDashboardServer
+	public static class TestMicroservicesDashboardServerApplication {
+
+		public static void main(String[] args) {}
+	}
+}
