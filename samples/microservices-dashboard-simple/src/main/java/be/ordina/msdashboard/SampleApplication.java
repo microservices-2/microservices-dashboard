e067575a6352e1a98ad936bd0509e797a1fe3273
Tim Ysewyn <Tim.Ysewyn@me.com>
2018-11-24 17:56:10 +0200

    Removed @EnableMicroservicesDashboardServer

--- samples/microservices-dashboard-simple/src/main/java/be/ordina/msdashboard/SampleApplication.java
+++ samples/microservices-dashboard-simple/src/main/java/be/ordina/msdashboard/SampleApplication.java
@@ -7,7 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * @author Tim Ysewyn
  */
 @SpringBootApplication
-@EnableMicroservicesDashboardServer
 public class SampleApplication {
 
 	public static void main(String[] args) {
